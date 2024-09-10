3ac985a1b0a3faf86160f6758e157f6f32ff2bcf
Tud8r <tudorceclan28@gmail.com>
2024-09-11 00:55:50 +0300

    change

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Pedometer } from 'expo-sensors';
 import CircularProgress from 'react-native-circular-progress-indicator';
+import Ionicons from "react-native-vector-icons/Ionicons";
+import {
+  useFonts,
+  Fredoka_400Regular,
+  Fredoka_500Medium,
+  Fredoka_600SemiBold,
+  Fredoka_700Bold,
+} from "@expo-google-fonts/fredoka";
+
+const background = require('./assets/background.jpeg');;
 
 export default function App() {
+
   const [isPedometerAvailable, setIsPedometerAvailable] = useState('checking');
   const [currentStepCount, setCurrentStepCount] = useState(0);
+
 
   // Function to simulate step count increment
   const simulateSteps = () => {
@@ -14,6 +26,7 @@ export default function App() {
     }, 10);
 
     return intervalId;
+
   };
 
   const subscribe = async () => {
@@ -25,6 +38,7 @@ export default function App() {
         // Watch for step count changes
         const subscription = Pedometer.watchStepCount(result => {
           console.log('Current step count:', result.steps); // Debugging output
+
           setCurrentStepCount(result.steps);
         });
 
@@ -36,6 +50,9 @@ export default function App() {
       console.error("Error with pedometer:", error);
       setIsPedometerAvailable('not available');
     }
+    console.log('Is pedometer available:', isPedometerAvailable);
+
+
   };
 
   useEffect(() => {
@@ -47,46 +64,112 @@ export default function App() {
 
     return () => {
       // Clean up pedometer subscription
-      
+
       // Clear interval when component unmounts
       clearInterval(intervalId);
     };
-  }, []);
 
+  }, []);
+  let [fontsLoaded] = useFonts({
+    Fredoka_400Regular,
+    Fredoka_500Medium,
+    Fredoka_600SemiBold,
+    Fredoka_700Bold,
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Pedometer.isAvailableAsync(): {isPedometerAvailable}</Text>
-      <Text>Current step count: {currentStepCount}</Text>
-      <View>
-        <CircularProgress
-          value={currentStepCount}
-          maxValue={100}
-          radius={120}
-          textColor={'#351F17'}
-          activeStrokeColor={'#351F17'}
-          inActiveStrokeColor={'#BCCCBF'}
-          progressValueColor={'#351F17'}
-          inActiveStrokeOpacity={0.5}
-          inActiveStrokeWidth={40}
-          activeStrokeWidth={40}
-          strokeLinecap = {'butt'}
+    <ImageBackground source={background} resizeMode="cover" style={{ flex: 1, justifyContent: 'center' }}>
+
+
+      <View style={styles.container}>
+      <TouchableOpacity
+            style={{
+              position: 'absolute', 
+              borderWidth: 1,
+              borderColor: 'rgba(0,0,0,0.2)',
+              alignItems: 'center',
+              alignSelf: 'flex-start',
+              justifyContent: 'center',
+              top:25,
+              left:10,
+              width: 45,
+              height: 45,
+              backgroundColor: '#4A5B6D',
+              borderRadius: 50,
+            }}
+          >
+            <Ionicons name="menu-outline" size={30} color="#FFFFFF" />
+          </TouchableOpacity>
+        <View style={styles.UpperContainer}>
           
           
-          dashedStrokeConfig={{
-            count: 10,
-            width: 80,
-          }}
-        />
+          <Text style={{ color: '#FFFFFF', fontFamily: 'Fredoka_600SemiBold', fontSize: 30 }}>Health Tracker</Text>
+          <Text></Text>
+          <Text></Text>
+
+          <Text style={{ color: '#FFFFFF', fontFamily: 'Fredoka_600SemiBold', fontSize: 20 }}>Great job! You've walked {currentStepCount}</Text>
+          <Text style={{ color: '#FFFFFF', fontFamily: 'Fredoka_600SemiBold', fontSize: 20 }}>steps today!</Text>
+        </View>
+        <View style={styles.Progress}>
+          <CircularProgress
+
+            value={currentStepCount}
+            maxValue={100}
+            radius={150}
+            title='steps'
+            titleStyle={{ color: '#FFFFFF', fontFamily: 'Fredoka_600SemiBold', fontSize: 30 , bottom:20}}
+            titleColor={'#FFFFFF'}
+            textColor={'#FFFFFF'}
+            activeStrokeColor={'#5C8646'}
+            inActiveStrokeColor={'#BCCCBF'}
+            progressValueColor={'#FFFFFF'}
+            inActiveStrokeOpacity={0.5}
+            inActiveStrokeWidth={40}
+            activeStrokeWidth={40}
+            strokeLinecap={'butt'}
+
+
+            dashedStrokeConfig={{
+              count: 10,
+              width: 100,
+            }}
+          />
+        </View>
+        <View style={styles.BottomContainer}>
+
+          <Text style={{ color: '#FFFFFF', fontFamily: 'Fredoka_600SemiBold', fontSize: 20, textAlign: 'center', }}>Every step counts towards a healthier you and a greener planet. You've earned  ?? eco-point!</Text>
+        </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 15,
+
     alignItems: 'center',
     justifyContent: 'center',
   },
+  UpperContainer: {
+    flex: 2,
+    marginTop: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  BottomContainer: {
+    flex: 2,
+    alignItems: 'center',
+    textAlign: 'center',
+    justifyContent: 'flex-start',
+
+  },
+  Progress: {
+    flex: 5,
+    justifyContent: 'center',
+  }
+
 });
